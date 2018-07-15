897a414d6b4e51a4d8903b26115f58627c80c5d1
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-15 20:16:36 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split(/items\//).last
+      result = search_item_and_get_price(item)
+      if result == nil
+        result = "Item not found"
+        resp.status = 400
+      end
+      resp.write result
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+
+  def search_item_and_get_price(item_name)
+    item = @@items.find {|i| i.name == item_name}
+    item != nil ? "#{item.price}" : nil
+  end
+
+end
